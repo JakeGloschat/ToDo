6eab69a3a5d40f50260478a89b38114554023ec6
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-02-19 12:59:11 -0700

    Got my storyboard looking sexy.  Now time to actually start writing some code.

---
+++ Unit.3.Assessment.ToDo/Unit.3.Assessment.ToDo/Models/NewToDo.swift
@@ -0,0 +1,13 @@
+//
+//  NewToDo.swift
+//  Unit.3.Assessment.ToDo
+//
+//  Created by Jake Gloschat on 2/19/23.
+//
+
+import Foundation
+
+class NewToDo {
+    
+    
+}
---
+++ Unit.3.Assessment.ToDo/Unit.3.Assessment.ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unit.3.Assessment.ToDo
+//
+//  Created by Jake Gloschat on 2/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
